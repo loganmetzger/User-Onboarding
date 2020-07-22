00d651f6787a43ff6dc3289642aeb015548b238b
Logan Metzger <loganmetzger@Logan-Metzger.local>
2020-07-22 16:06:55 -0400

    put in some basic JSX on app so there's something to return. declared empty strings for initial form values

--- onboarding/src/App.js
+++ onboarding/src/App.js
@@ -3,13 +3,39 @@ import './App.css';
 import axios from 'axios'
 import * as yup from 'yup'
 import formSchema from './formSchema'
-import userForm from './userForm'
+import UserForm from './UserForm'
 import User from './User'
+
+// Initial form values as empty strings of false for checkbox
+const initialFormValues = {
+  name: '', 
+  email: '',
+  password: '',
+  terms: {
+    accepted: false
+  }
+}
+
+
+
 
 function App() {
   return (
     <div className="App">
-      
+      <header><h1>User Onboarding</h1></header>
+
+      <userForm 
+        // props go here
+      />
+      {
+        users.map(user => {
+          return (
+            <User 
+              // props go here
+            />
+          )
+        })
+      }
     </div>
   );
 }
